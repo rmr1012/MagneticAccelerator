89f2c74ea6002714b29035b4e5116e12eca54ed9
JackShi <jackshi0912@gmail.com>
2019-05-18 15:14:50 -0700

    Work in progress on simulation.

--- descrete_optimization/capacitor.py
+++ descrete_optimization/capacitor.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
+from typing import List
 
 IntTensorVar = tf.Variable
 FloatTensorVar = tf.Variable
 IntTensor = tf.Tensor
 FloatTensor = tf.Tensor
+FloatTimeSeriesTensor = tf.Tensor  # Dtype tf.float, shape [num_time_steps]
 
 
 class Capacitor:
@@ -20,9 +22,16 @@ class Capacitor:
         self.esr: float = esr
         self.initial_voltage: float = initial_voltage
         self.capacitance: float = capacitance
+        self.energy: float = 0.5 * (
+                self.capacitance * 10 ** -6) * self.initial_voltage ** 2
 
-    def get_voltage(self, t: IntTensor) -> float:
-        return self.initial_voltage
-
-    def get_energy(self, t: IntTensor) -> float:
-        return 0.5 * (self.capacitance * 10 ** -6) * self.get_voltage(t) ** 2
+    @tf.function
+    def discretized_current_curve(self, time_range: List[int],
+                                  inductance: FloatTensor,
+                                  fundamental_frequency: FloatTensor,
+                                  dampening_factor: FloatTensor) \
+            -> FloatTimeSeriesTensor:
+        constant: FloatTensor = self.initial_voltage / (
+                inductance * 10 ** -6 * fundamental_frequency)
+        return constant * tf.exp(-dampening_factor * time_range) * tf.sin(
+            fundamental_frequency * time_range)
--- descrete_optimization/stage.py
+++ descrete_optimization/stage.py
@@ -1,6 +1,12 @@
 from descrete_optimization import Solenoid
 from descrete_optimization import Capacitor
 from descrete_optimization import Projectile
+from typing import Tuple, List
+import tensorflow as tf
+
+IntTensor = tf.Tensor
+FloatTensor = tf.Tensor
+FloatTimeSeriesTensor = tf.Tensor  # Dtype tf.float, shape [num_time_steps]
 
 
 class Stage:
@@ -10,6 +16,41 @@ class Stage:
         self._capacitor = capacitor
         self._projectile = projectile
 
-    def simulate(self, duration, runcycle):
+    @tf.function
+    def calculate_efficiency(self, dt: float,
+                             num_intervals: int) -> FloatTensor:
         pass
 
+    @tf.function
+    def _damping_factor_op(self) -> FloatTensor:
+        return (self._capacitor.esr + self._solenoid.resistance) / (
+                2 * self._solenoid.inductance)
+
+    @tf.function
+    def _fundamental_frequency_op(self) -> FloatTensor:
+        return 1 / tf.sqrt(
+            self._solenoid.inductance * self._capacitor.capacitance)
+
+    @tf.function
+    def _b_op(self, fundamental_frequency: FloatTensor, inductance: FloatTensor,
+              init_cap_voltage: float) -> Tuple[FloatTensor, FloatTensor]:
+        return (tf.constant(0, dtype=tf.float32),
+                init_cap_voltage / (fundamental_frequency * inductance))
+
+    @tf.function
+    def _discretized_flywheel_current_curve(self, time_range: List[int],
+                                            fundamental_frequency: FloatTensor,
+                                            dampening_factor: FloatTensor) \
+            -> FloatTimeSeriesTensor:
+        Icyc: FloatTimeSeriesTensor = self._capacitor.discretized_current_curve(
+            time_range,
+            self._solenoid.inductance,
+            fundamental_frequency,
+            dampening_factor)
+
+        Idis: FloatTimeSeriesTensor = tf.math.reduce_max(Icyc) * tf.exp(
+            (-self._solenoid.resistance + time_range) / (
+                    self._solenoid.inductance * 10 ** -6))
+        local_max_idx: IntTensor = tf.argmax(Icyc, axis=0)
+
+        return tf.concat([Icyc[:local_max_idx], Idis[local_max_idx:]], axis=0)
