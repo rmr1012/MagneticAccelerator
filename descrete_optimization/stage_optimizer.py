72a2cc63197156157ad02c0251824e91ba936299
JackShi <jackshi0912@gmail.com>
2019-06-16 14:22:47 -0700

    Fixed another bug. Width becomes a non-factor

--- descrete_optimization/stage_optimizer.py
+++ descrete_optimization/stage_optimizer.py
@@ -5,7 +5,8 @@ from descrete_optimization import Capacitor, Projectile, Solenoid, Stage
 
 class OptimizationParams(NamedTuple):
     num_layers: tf.Tensor
-    coil_width: tf.Tensor
+    coil_width: tf.Variable
+    capacitant: tf.Variable
     inner_dia: tf.Tensor
     gauge: tf.Tensor
     stage: Stage
@@ -13,16 +14,33 @@ class OptimizationParams(NamedTuple):
 
 def create_stage() -> OptimizationParams:
     num_layers: tf.Tensor = tf.random.uniform([], minval=1, maxval=10, dtype=tf.int32)
-    coil_width: tf.Tensor = tf.random.uniform([], minval=0.01, maxval=0.5, dtype=tf.float32)
+    coil_width: tf.Variable = tf.Variable(tf.random.uniform([], minval=0.01, maxval=0.5, dtype=tf.float32),
+                                          trainable=True)
+    capacitant: tf.Variable = tf.Variable(tf.random.uniform([], minval=10 ** -7, maxval=10 ** -4, dtype=tf.float32),
+                                          trainable=True)
     inner_dia: tf.Tensor = tf.constant(0.005, dtype=tf.float32)
     gauge: tf.Tensor = tf.random.uniform([], minval=1, maxval=41, dtype=tf.int32)
     solenoid = Solenoid(num_layers, coil_width, inner_dia, gauge)
-    capacitor = Capacitor(15, 650 * 10 ** -6)
+    capacitor = Capacitor(20, capacitant)
     projectile = Projectile(0.015, 0.005)
     stage: Stage = Stage(solenoid, capacitor, projectile, 0.001)
-    return OptimizationParams(num_layers, coil_width, inner_dia, gauge, stage)
+    return OptimizationParams(num_layers, coil_width, capacitant, inner_dia, gauge, stage)
+
+
+def train_step(optimizer, opt_params):
+    with tf.GradientTape() as tape:
+        loss = 1 - opt_params.stage.calculate_efficiency(0.25, 2500)
+    gradients = tape.gradient(loss, [opt_params.coil_width])
+    optimizer.apply_gradients(zip(gradients, [opt_params.coil_width]))
+    return loss
 
 
 if __name__ == "__main__":
-    opt_params: OptimizationParams = create_stage()
-    print([opt_params.stage.calculate_efficiency(0.25, 2500).result() for i in range(20)])
+    NUM_HPARAM_SEARCH = 50
+    NUM_TRAIN_STEPS = 20
+    for i in range(NUM_HPARAM_SEARCH):
+        optimizer = tf.keras.optimizers.Adam(10.1)
+        opt_params: OptimizationParams = create_stage()
+        for i in range(NUM_TRAIN_STEPS):
+            loss = train_step(optimizer, opt_params)
+            print("step ", i, " - loss: ", loss.numpy(), "\t coil_width: ", opt_params.coil_width.numpy())
